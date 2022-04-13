5e4b34f73f1b229d58ffdaf87705ff26c6098356
bariscankurtkaya <bariscankurtkaya@gmail.com>
2022-04-13 22:09:28 +0300

    Charmander 🔥

--- FullStack-Developer-101/Frontend/src/app/app.component.spec.ts
+++ FullStack-Developer-101/Frontend/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SkyLab'`, () => {
+  it(`should have as title 'Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SkyLab');
+    expect(app.title).toEqual('Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SkyLab app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Frontend app is running!');
   });
 });
