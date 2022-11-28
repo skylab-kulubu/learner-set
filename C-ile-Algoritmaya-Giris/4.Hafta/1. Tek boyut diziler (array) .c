c861f7c977102ea9b6ddbf2148bdb7e67cac1970
Hasan Basri Uzun <uzun.hbasri@gmail.com>
2022-11-28 18:45:56 +0300

    c ile algoritmaya giriş

---
+++ C-ile-Algoritmaya-Giris/4.Hafta/1. Tek boyut diziler (array) .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int dizi [5] = {17,34,98,193,6589};
+    printf("3. indis: %d\n",dizi[3]);
+    printf("0. indis: %d\n",dizi[0]);
+    printf("4. indis: %d\n",dizi[4]);
+
+    dizi_adi[0] ---------> dizinin 1.elemanı
+    dizi_adi[1] ---------> dizinin 2.elemanı
+    dizi_adi[2] ---------> dizinin 3.elemanı
+
+    return 0;
+}
